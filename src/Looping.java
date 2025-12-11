5820e2c7b9d358ac0a11b38268aea59be1152aa7
user.email <rajeshramdeveee56@gmail.com>
2025-12-11 18:00:08 +0530

    FirstCommit

---
+++ src/Looping.java
@@ -0,0 +1,16 @@
+public class Looping {
+        public void forLoop()
+        {
+            for(int i=1; i<=5; i++)
+            {
+                System.out.println(i);
+            }
+        }
+
+        public static void main(String[] args) {
+            Looping lp = new Looping();
+            lp.forLoop();
+        }
+    }
+
+
