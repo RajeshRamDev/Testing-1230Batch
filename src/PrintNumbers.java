2a77cfdf867b924b6e31059584f0818696de3fef
user.email <rajeshramdeveee56@gmail.com>
2025-12-13 12:08:20 +0530

    PrintNumbers

---
+++ src/PrintNumbers.java
@@ -0,0 +1,16 @@
+public class PrintNumbers {
+    public void printNumbers()
+    {
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        PrintNumbers pn = new PrintNumbers();
+        pn.printNumbers();
+    }
+}
+
