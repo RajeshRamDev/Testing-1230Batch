268a470978b1c035ad701d54ad444f1ea5de3faf
user.email <rajeshramdeveee56@gmail.com>
2025-12-20 16:01:13 +0530

    SWITCH and BREAK statements2 Print Day of the Week

---
+++ src/DayofWeek.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class DayofWeek {
+    Scanner sc = new Scanner(System.in);
+    int choice;
+    public void dayChecker()
+    {
+        System.out.println("Enter the choice: 1 for Monday,2 for Tuesday,3 for Wednesday,4 for Thursday,5 for Friday,6 for Saturday,7 for Friday");
+       choice = sc.nextInt();
+       switch(choice)
+       {
+           case 1:
+               System.out.println("Monday");
+               break;
+           case 2:
+               System.out.println("Tuesday");
+               break;
+           case 3:
+               System.out.println("Wednesday");
+               break;
+           case 4:
+               System.out.println("Thursday");
+               break;
+           case 5:
+               System.out.println("Friday");
+               break;
+           case 6:
+               System.out.println("Saturday");
+               break;
+           case 7:
+               System.out.println("Sunday");
+               break;
+
+       }
+    }
+
+    public static void main(String[] args) {
+        DayofWeek dw = new DayofWeek();
+        dw.dayChecker();
+    }
+}
