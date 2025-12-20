619c53b52aa9d869a85f997b93f829bf4941c3ae
user.email <rajeshramdeveee56@gmail.com>
2025-12-20 13:13:35 +0530

    Constructor1 Default Constructor

---
+++ src/AnimalCons.java
@@ -0,0 +1,12 @@
+public class AnimalCons {
+    public AnimalCons(String S)
+    {
+        System.out.println(S);
+    }
+
+    public static void main(String[] args) {
+        AnimalCons al = new AnimalCons("Animal is Created");
+    }
+}
+
+
