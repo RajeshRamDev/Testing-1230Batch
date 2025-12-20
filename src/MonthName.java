fb2efef6197c4b1d702dea73c7c7e708520aca1a
user.email <rajeshramdeveee56@gmail.com>
2025-12-20 16:48:11 +0530

    SWITCH and BREAK statements5 Month Name

---
+++ src/MonthName.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class MonthName {
+    Scanner sc = new Scanner(System.in);
+    int choice;
+    public void monthChecker()
+    {
+        System.out.println("Enter your choice for the month from 1 to 12: ");
+        choice = sc.nextInt();
+        switch (choice)
+        {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+                case 12:
+            System.out.println("December");
+            break;
+
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        MonthName mn = new MonthName();
+        mn.monthChecker();
+    }
+}
