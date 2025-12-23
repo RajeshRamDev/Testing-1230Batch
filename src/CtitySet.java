74d0aa210e1ad1f37efb76574d122980c3c92be9
user.email <rajeshramdeveee56@gmail.com>
2025-12-23 16:47:25 +0530

    Collections Easy 2.Hash Set

---
+++ src/CtitySet.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CtitySet {
+    Set<String>city = new HashSet<>();
+    public void cityMethod()
+    {
+        city.add("Chennai");
+        city.add("Bengaluru");
+        city.add("Mumbai");
+        city.add("Kochi");
+        city.add("Hyderabad");
+        System.out.println("List of Cities");
+        for(String s: city)
+        {
+            System.out.println(s);
+        }
+    }
+
+    public static void main(String[] args) {
+        CtitySet cs = new CtitySet();
+        cs.cityMethod();
+    }
+}
